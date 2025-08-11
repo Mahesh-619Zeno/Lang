863fc1f790bb307ea493deb542886720018fd310
Mahesh-619Zeno <mahesh.ghadei@techolution.com>
2025-08-11 11:47:55 +0530

    feat: new typescript file added with some other changes

--- Java/BubbleSort.java
+++ Java/BubbleSort.java
@@ -2,14 +2,14 @@ import java.util.Arrays;
 
 public class BubbleSort {
     public static void main(String[] args) {
-        int[] arr = {5, 3, 8, 6, 2};
-        for (int i = 0; i < arr.length - 1; i++) {
-            for (int j = 0; j < arr.length - i - 1; j++) {
-                if (arr[j] > arr[j+1]) {
-                    int temp = arr[j]; arr[j] = arr[j+1]; arr[j+1] = temp;
+        int[] numbers = {5, 3, 8, 6, 2};
+        for (int i = 0; i < numbers.length - 1; i++) {
+            for (int j = 0; j < numbers.length - i - 1; j++) {
+                if (numbers[j] > numbers[j+1]) {
+                    int temp_value = numbers[j]; numbers[j] = numbers[j+1]; numbers[j+1] = temp_value;
                 }
             }
         }
-        System.out.println("Sorted: " + Arrays.toString(arr));
+        System.out.println("Sorted: " + Arrays.toString(numbers));
     }
 }
